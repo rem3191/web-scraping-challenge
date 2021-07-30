8a701a2f4dd3c3869d921c74bdcabd3e27d59212
ROBYN MILLER <REM3191@HOTMAIL.COM>
2021-07-30 19:12:12 -0400

    updated hw

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, redirect, url_for
 from flask_pymongo import PyMongo
 import scrape_mars
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="templates")
 
 # Use flask_pymongo to set up mongo connection
 app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
--- scrape_mars.py
+++ scrape_mars.py
@@ -1,32 +1,102 @@
+from splinter import Browser
+from bs4 import BeautifulSoup as soup
+import pandas as pd
 import datetime as dt
+from webdriver_manager.chrome import ChromeDriverManager
 
 def scrape_all():
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+    browser = Browser('chrome', **executable_path, headless=False)
+    
+    # Visit the mars nasa news site
+    url = 'https://redplanetscience.com/'
+    browser.visit(url)
 
-    news_title = 'Testing Title'
-    news_paragraph = 'Testing Paragraph'
-    featured_image = 'http://peoplelovescience.com/wp-content/uploads/2017/08/21151729_1326742644090307_971208679029560636_n-2.jpg'
-    mars_facts = 'Testing Facts Table'
+    # Optional delay for loading the page
+    browser.is_element_present_by_css('div.list_text', wait_time=1)
 
-    hemispheres_list_of_dicts = [
-        {"title": "Title of Hemisphere Image",
-        "img_url": 'http://peoplelovescience.com/wp-content/uploads/2017/08/21151729_1326742644090307_971208679029560636_n-2.jpg'},
+    # Convert the browser html to a soup object and then quit the browser
+    html = browser.html
+    news_soup = soup(html, 'html.parser')
 
-        {"title": "Title of Hemisphere Image",
-        "img_url": 'http://peoplelovescience.com/wp-content/uploads/2017/08/21151729_1326742644090307_971208679029560636_n-2.jpg'},
-
-        {"title": "Title of Hemisphere Image",
-        "img_url": 'http://peoplelovescience.com/wp-content/uploads/2017/08/21151729_1326742644090307_971208679029560636_n-2.jpg'},
+    slide_elem = news_soup.select_one('div.list_text')
         
-        {"title": "Title of Hemisphere Image",
-        "img_url": 'http://peoplelovescience.com/wp-content/uploads/2017/08/21151729_1326742644090307_971208679029560636_n-2.jpg'},
-    ]
+    slide_elem.find('div', class_='content_title')
+
+    # Use the parent element to find the first a tag and save it as `news_title`
+    news_title = slide_elem.find('div', class_='content_title').get_text()
+    # Use the parent element to find the paragraph text
+    news_p = slide_elem.find('div', class_='article_teaser_body').get_text()
+    # Visit URL
+    url = 'https://spaceimages-mars.com'
+    browser.visit(url)
+
+    # Find and click the full image button
+    full_image_elem = browser.find_by_tag('button')[1]
+    full_image_elem.click()
+
+    # Parse the resulting html with soup
+    html = browser.html
+    img_soup = soup(html, 'html.parser')
+
+    # find the relative image url
+    img_url_rel = img_soup.find('img', class_='fancybox-image').get('src')
+
+    # Use the base url to create an absolute url
+    img_url = f'https://spaceimages-mars.com/{img_url_rel}'
+
+    df = pd.read_html('https://galaxyfacts-mars.com')[0]
+
+    df.columns=['Description', 'Mars', 'Earth']
+    df.set_index('Description', inplace=True)
+
+    mars_facts=df.to_html()
+
+    # 1. Use browser to visit the URL 
+    url='https://marshemispheres.com/'
+    # url = 'https://astrogeology.usgs.gov/'
+
+    browser.visit(url)
+
+    html_hemispheres = browser.html
+    hemispheres_bs=soup(html_hemispheres,'html.parser')
+
+    results=hemispheres_bs.find('div', class_='result-list')
+    hemispheres=results.find_all('div',{'class':'item'})
+
+    # 2. Create a list to hold the images and titles.
+    hemisphere_image_urls=[]
+    hemisphere={}
+
+    # 3. Write code to retrieve the image urls and titles for each hemisphere.
+
+    for hemisphere in hemispheres:
+    # hemisphere=hemispheres[0]
+        title = hemisphere.find("h3").text
+        end_link = hemisphere.find("a")["href"]
+        hemisphere_link = "https://marshemispheres.com/" + end_link    
+        browser.visit(hemisphere_link)
+
+        hemisphere_html = browser.html
+        hemisphere_soup=soup(hemisphere_html, "html.parser")
+        hemisphere_image_link = hemisphere_soup.find("img",{"class":"wide-image"})["src"]
+        hemisphere_wide_image_link = "https://marshemispheres.com/" + hemisphere_image_link
+        hemisphere_dict={
+            "title":title,
+            "image_url":hemisphere_wide_image_link
+            }
+        hemisphere_image_urls.append(hemisphere_dict)
+        
+        # 5. Quit the browser
+    browser.quit()
+    
 
     scraped_data = {
         "news_title": news_title,
-        "news_paragraph": news_paragraph,
-        "featured_image": featured_image,
+        "news_paragraph": news_p,
+        "featured_image": img_url,
         "facts": mars_facts,
-        "hemispheres": hemispheres_list_of_dicts,
+        "hemispheres": hemisphere_image_urls,
         "last_modified": dt.datetime.now()
     }
 
